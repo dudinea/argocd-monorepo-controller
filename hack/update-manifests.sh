4565093ae0349aca16128d1703301a844bb9276f
Eugene Doudine <eugene.doudine@octopus.com>
2025-12-04 01:06:41 +0200

    fix installation manifests variable usage
    Signed-off-by: Eugene Doudine <eugene.doudine@octopus.com>

--- hack/update-manifests.sh
+++ hack/update-manifests.sh
@@ -3,14 +3,13 @@ set -x
 set -o errexit
 set -o nounset
 set -o pipefail
+set -e
 
 SRCROOT="$( CDPATH='' cd -- "$(dirname "$0")/.." && pwd -P )"
 AUTOGENMSG="# This is an auto-generated file. DO NOT EDIT"
 
 KUSTOMIZE=kustomize
 [ -f "$SRCROOT/dist/kustomize" ] && KUSTOMIZE="$SRCROOT/dist/kustomize"
-
-cd ${SRCROOT}/manifests/ha/base/redis-ha && ./generate.sh
 
 IMAGE_NAMESPACE="${IMAGE_NAMESPACE:-quay.io/argoprojlabs}"
 IMAGE_TAG="${IMAGE_TAG:-}"
@@ -34,7 +33,9 @@ which $KUSTOMIZE
 cd ${SRCROOT}/manifests/base && $KUSTOMIZE edit set image quay.io/argoprojlabs/argocd-monorepo-controller=${IMAGE_NAMESPACE}/argocd-monorepo-controller:${IMAGE_TAG}
 
 echo "${AUTOGENMSG}" > "${SRCROOT}/manifests/install.yaml"
-$KUSTOMIZE build "${SRCROOT}/manifests/base" >> "${SRCROOT}/manifests/install.yaml"
+$KUSTOMIZE build "${SRCROOT}/manifests/cluster-install" >> "${SRCROOT}/manifests/install.yaml"
 
+echo "${AUTOGENMSG}" > "${SRCROOT}/manifests/namespace-install.yaml"
+$KUSTOMIZE build "${SRCROOT}/manifests/namespace-install" >> "${SRCROOT}/manifests/namespace-install.yaml"
 
 
